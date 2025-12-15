622b555bce1eb84a09de9e7b6ad26354cc0be943
cjmeados <meadoschris@gmail.com>
2025-12-14 18:32:03 -0600

    fleshes out greetController.js

---
+++ controllers/greetController.js
@@ -0,0 +1,20 @@
+function greetController(req, res, next) {
+    try {
+        const name = req.query.name;
+
+        if (!name) {
+            return next({status: 400, message: 'Name is required'});
+        }
+
+        const greeting = `Hello ${name}`;
+
+        console.log('Controller processed greeting', greeting); // console log 
+
+        res.status(200).json({greeting});
+
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = greetcontroller;
