34051dcaa464c42371138b63a5db688bd51d8a5e
cjmeados <meadoschris@gmail.com>
2025-12-18 01:22:16 -0600

    moves html,css,js to public folder

---
+++ public/script.js
@@ -0,0 +1,16 @@
+let apiBtn = document.getElementById('apiBtn');
+let landingOutput = document.getElementById('landingOutput');
+
+apiBtn.addEventListener('click', async () => { //look into async more later
+    const response = await fetch('/hello'); //look into let/const again and await
+
+    const data = await response.json();
+
+    // add error handle later
+
+    landingOutput.textContent = data.message;
+
+
+
+
+});
